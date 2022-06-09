8d1ae100b1c95822f5e1e46233bd5ca4412d9103
PercTruman <drshawnhart@gmail.com>
2022-06-08 19:33:55 -0500

    navLinks looking a little better

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,14 @@ import { NavLink } from "react-router-dom"
 function NavBar() {
   return (
     <div>
-        <header>
+      <header>
         <h1>Easy Money: Finally, A Fundraiser That Works As Hard As You Do</h1>
         <nav>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/mycontacts">My Contacts</NavLink>
-          <NavLink to="/newdonorform">Add New Donor</NavLink>
+          <NavLink to="/login" className="headerLink">Login</NavLink>
+          <NavLink to="/mycontacts" className="headerLink">My Contacts</NavLink>
+          <NavLink to="/newdonorform" className="headerLink">Add New Donor</NavLink>
         </nav>
-    </header>
+      </header>
     </div>
   )
 }
