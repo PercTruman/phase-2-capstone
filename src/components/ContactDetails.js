e33b929ce8713de62cddf805635178f7f7c7ce2d
PercTruman <drshawnhart@gmail.com>
2022-06-10 08:38:33 -0500

    getting contact detail pages rendering

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import  { useParams } from "react-router-dom"
 
 function ContactDetails() {
-  const [detailedCard, setDetailedCard] = useState(null);
+  const [detailedCard, setDetailedCard] = useState('hi');
   const { id } = useParams()
   
   useEffect(()=> {
@@ -11,17 +11,15 @@ function ContactDetails() {
         .then(contactObject=>setDetailedCard(contactObject))
   }, [id])
 
-  console.log(detailedCard)
-
   return (
     <div className="cardContainer">
       <div className="card">
        
-        <h4>Name: {detailedCard}</h4> 
-        {/* <h4>Email: {email}</h4> */}
-        {/* <h4>Phone: {phone}</h4>
-        <h4>Donated? {hasDonated ? "Yes" : "No"}</h4>
-        <h4>Amount of donation: {amountDonated}</h4> */}
+        <h4>Name: {detailedCard.firstname + detailedCard.lastname}</h4> 
+        <h4>Email: {detailedCard.email}</h4>
+        <h4>Phone: {detailedCard.phonenumber}</h4>
+        <h4>Donated? {detailedCard.hasDonated ? "Yes" : "No"}</h4>
+        <h4>Amount of donation: ${detailedCard.amountDonated} </h4> 
       </div>
     </div>
   );
