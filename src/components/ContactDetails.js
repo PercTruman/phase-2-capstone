31f7e3f3ca5e32ef28923be2d9dfbe4020d905ee
PercTruman <drshawnhart@gmail.com>
2022-06-10 08:55:36 -0500

    fixing more formatting in contact cards

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import  { useParams } from "react-router-dom"
+import  { useParams, Link } from "react-router-dom"
+
 
 function ContactDetails() {
   const [detailedCard, setDetailedCard] = useState('hi');
@@ -13,13 +14,14 @@ function ContactDetails() {
 
   return (
     <div className="cardContainer">
-      <div className="card">
+      <div className="bigcard">
        
         <h4>Name: {detailedCard.firstname + detailedCard.lastname}</h4> 
         <h4>Email: {detailedCard.email}</h4>
         <h4>Phone: {detailedCard.phonenumber}</h4>
         <h4>Donated? {detailedCard.hasDonated ? "Yes" : "No"}</h4>
         <h4>Amount of donation: ${detailedCard.amountDonated} </h4> 
+        <Link to = {'/mycontacts'}>Back to Contacts</Link>
       </div>
     </div>
   );
