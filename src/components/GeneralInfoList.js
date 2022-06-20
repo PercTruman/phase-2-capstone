48c65fda130523be67b31567b92e7b2663989596
PercTruman <drshawnhart@gmail.com>
2022-06-20 14:10:45 -0500

    updating NaVBar

--- src/components/GeneralInfoList.js
+++ src/components/GeneralInfoList.js
@@ -1,43 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Grid }from "@mui/material";
-import { Paper } from "@mui/material"
+import { Grid } from "@mui/material";
+import { Paper } from "@mui/material";
 
 function GeneralInfoList({ filteredContacts }) {
-  
   const list = filteredContacts.map((card) => (
     <Grid item xs={4}>
-    <Paper 
-      key={card.id}
-    >
-      <div>
-        <h4>
-          {card.firstname} {card.lastname}
-        </h4>
-        <h4>Donor? {card.hasDonated ? "Yes" : "No"}</h4>
-        <Link
-          style={{
-            display: "block",
-            paddingBottom: "15px",
-            textAlign: "center",
-          }}
-          to={`/contacts/${card.id}`}
-        >
-          Contact Details
-        </Link>
-      </div>
-    </Paper>
+      <Paper
+        elevation={18}
+        style={{ margin: "0px 0px 8px 0px", border: "1px solid black" }}
+        key={card.id}
+      >
+        <div>
+          <h4>
+            {card.firstname} {card.lastname}
+          </h4>
+          <h4>Donor? {card.hasDonated ? "Yes" : "No"}</h4>
+          <Link
+            style={{
+              display: "block",
+              paddingBottom: "15px",
+              textAlign: "center",
+            }}
+            to={`/contacts/${card.id}`}
+          >
+            Contact Details
+          </Link>
+        </div>
+      </Paper>
     </Grid>
-    
   ));
 
   return (
     <div className="cardContainer">
-      <Grid container spacing = {3}>
-        
-          
-          {list}
-  
+      <Grid container spacing={3}>
+        {list}
       </Grid>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,17 @@ import { AppBar, Toolbar, Typography, Stack } from "@mui/material";
 
 function NavBar() {
   const titleStyle={
-    width: "100%",
-    margin: "0 auto",
+    width: "40%",
     fontSize: "32px",
-    display: "block"
+    display: "block",
+    padding: "30px"
   }
   const linkStyle={
     textDecoration: "none",
     color: "yellow"
   }
   return (
-    <AppBar position="static" sx={{height:"6rem"}}>
+    <AppBar position="static" sx={{height:"8rem"}}>
       <Toolbar>
         <img src = ".../public/train.jpg" alt="train logo"/>
         <Typography style={titleStyle} variant="h5" component="div" sx={{ flexGrow: 1 }}>
